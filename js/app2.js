fb586f073aa34629b86779b1757cdb625bb28110
Peter Kolomyichenkoh <p.kolomiychenko@webversor.com>
2017-10-20 20:27:13 +0300

    mingwe starting calendar

---
+++ js/app2.js
@@ -0,0 +1,53 @@
+var myMonths = [
+  {
+    name: 'Jan'
+  },
+  {
+    name: 'Feb'
+  },
+  {
+    name: 'Mar'
+  },
+  {
+    name: 'Apr'
+  },
+  {
+    name: 'May'
+  },
+  {
+    name: 'Jun'
+  },
+  {
+    name: 'Jul'
+  },
+  {
+    name: 'Aug'
+  },
+  {
+    name: 'Sep'
+  },
+  {
+    name: 'Oct'
+  },
+  {
+    name: 'Nov'
+  },
+  {
+    name: 'Dec'
+  }
+];
+
+var App = React.createClass({
+
+	render: function() {
+		return: (
+			<h1>Hello</h1>
+		)
+	}
+
+});
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
