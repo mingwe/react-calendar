9893a293b1395678b5c072adb9d2775a840c7da4
Peter Kolomiychenko <p.kolomiychenko@webversor.com>
2017-10-23 15:11:53 +0300

    calendar st1

--- README.md
+++
@@ -1,2 +0,0 @@
-# react-ru-tutorial
-Код для раздела [PropTypes](https://maxfarseer.gitbooks.io/react-course-ru/content/reactproptypes.html)
--- js/app2.js
+++ js/app2.js
@@ -45,28 +45,41 @@ var myMonths = [
 
 var myEvents = [
   {
-    1: {      
+    0: {
         title: 'Something happened',
         status: 1,
         link: 'google.com'
     },
-    4: {      
+    1: {
         title: 'another event',
         status: 2,
         link: 'yahoo.com'
     },
-    7: {      
+    2: {
         title: 'another bad event',
         status: 3,
         link: 'yahoo.com'
     },
-    10: {      
+    3: {
         title: 'third event',
         status: 3,
         link: 'youtube.com'
     }
   }
 ];
+
+function formatVal (val) {
+    if (val > 11) {
+        val = val - 12;
+    }
+    else {
+        if (val < 0) {
+            val = val + 12;
+        }
+    }
+    console.log(val);
+    return(val);
+};
 
 var App = React.createClass({
 
@@ -81,145 +94,201 @@ var App = React.createClass({
 
 });
 
-var dateNow = new Date();    
-var now = dateNow.getMonth();    
-var year = dateNow.getFullYear();    
-var thisDate = new Date(year, now);
+var dateNow = new Date();
 
+var monthCurrent = dateNow.getMonth();
+var yearCurrent = dateNow.getFullYear();
 
-var Month = React.createClass({  
+console.log(monthCurrent);
+console.log(yearCurrent);
 
-  getInitialState: function() {    
-    
+var Month = React.createClass({
+
+  getInitialState: function() {
     return {
-      current: now,
-      year: year,
-      day: thisDate.getDay(),
-      days: thisDate.getDaysInMonth()
+      monthCurrent: monthCurrent,
+      yearCurrent: yearCurrent,
+      DIMCurrent: dateNow.getDaysInMonth()
     };
   },
 
-  changeMonth: function(step, e) {
-    e.preventDefault();   
+    changeMonth: function(step, e) {
+      e.preventDefault();
 
-    var changedYear = this.state.year+step;
-
-      if ((this.state.current == 11) && (step == 1)) {        
-        var thisDate = new Date(changedYear, 0);
-
-        this.setState({          
-          current: 0,
-          year: changedYear
+      var monthChanged = formatVal(this.state.monthCurrent+step);
+      if (step > 0 && monthChanged < this.state.monthCurrent) {
+        this.setState({
+           yearCurrent: this.state.yearCurrent+1
         });
       }
-
-      else {
-
-        if ((this.state.current == 0) && (step == -1)) {
-          var thisDate = new Date(changedYear, 11);
-
-          this.setState({
-            current: 11,
-            year: changedYear
-          });
-        }
-
-        else {
-
-          var currentNext = this.state.current+step;
-          var thisDate = new Date(this.state.year, currentNext);
-
-          this.setState({
-            current: currentNext
-          });
-        }
+      if (step < 0 && monthChanged > this.state.monthCurrent) {
+        this.setState({
+           yearCurrent: this.state.yearCurrent-1
+        });
       }
+      var dateNew = new Date(this.state.yearCurrent, monthChanged);
+
       this.setState({
-        day: thisDate.getDay(),
-        days: thisDate.getDaysInMonth()
+          monthCurrent: monthChanged,
+          DIMCurrent: dateNew.getDaysInMonth()
       });
-  },
 
-  render: function() {  
+    },
 
-    var current = this.state.current;    
+  render: function() {
 
-    var days = dateNow.getDaysInMonth();
-    var monthnum = this.props.monthnum;    
-    var thismonth = this.props.monthnum[current].name;
+    // var current = this.state.current;
 
-    return (      
-      <div> 
-        <a href="#" onClick={this.changeMonth.bind(this, -1)} className={'testClass ' + (current > 0 ? 'active': 'not')}>prev</a>
-        <a href="#" onClick={this.changeMonth.bind(this, 1)} className={'testClass ' + (current < 11 ? 'active': 'not')}>next</a>   
-        <h2>{this.state.year}</h2>
-        <h1 days={days}>{thismonth}</h1>
-        <Calendar days={this.state.days} day={this.state.day}/>
+    // var days = dateNow.getDaysInMonth();
+    // var monthnum = this.props.monthnum;
+
+      var monthCurrent = this.state.monthCurrent;
+      var yearCurrent = this.state.yearCurrent;
+      var DIMCurrent = this.state.DIMCurrent;
+      var dateCurrent = new Date(yearCurrent, monthCurrent);
+
+      var monthPrev = this.state.monthCurrent-1;
+      var datePrev = new Date(yearCurrent, monthPrev);
+      var DIMPrev = datePrev.getDaysInMonth();
+
+      console.log('lets start!: ');
+      console.log(monthCurrent +''+ yearCurrent);
+      console.log(DIMCurrent);
+      console.log(datePrev);
+      console.log(DIMPrev);
+
+      let daysArray = [];
+
+      var dayOffset = dateCurrent.getDay() -1;
+      if (dayOffset == -1) {
+          dayOffset = 6;
+      }
+
+      if (monthCurrent == 0) {
+          var monthPrev = 12;
+          var yearPrev = yearCurrent - 1;
+      }
+      else {
+          var monthPrev = monthCurrent;
+          var yearPrev = yearCurrent;
+      }
+      for(var x = DIMPrev-dayOffset+1; x <= DIMPrev; x++) {
+        daysArray.push(
+            <div thedate={x+'.'+monthPrev+'.'+yearPrev} className='side-month' key={+('0.'+x)}>{x}.{monthPrev}</div>
+        );
+      }
+
+      for(var i = 1; i <= DIMCurrent; i++) {
+        daysArray.push(
+          <div thedate={i+'.'+(monthCurrent+1)+'.'+yearCurrent} key={i}>{i}.{monthCurrent}.{yearCurrent}</div>
+        );
+      }
+
+      let PCDCount = i+dayOffset-1;
+      let daysNext = (7*(Math.floor(PCDCount/7))+7)-PCDCount;
+
+      for(var y = 1; y <= daysNext; y++) {
+          daysArray.push(
+              <div thedate={y+'.'+(monthCurrent+2)+'.'+yearCurrent} className='side-month' key={+('1.'+y)}>{y}.{monthCurrent+1}</div>
+          );
+      }
+
+
+    return (
+      <div>
+        <a href="#" onClick={this.changeMonth.bind(this, -1)} className={'testClass'}>prev</a>
+        <a href="#" onClick={this.changeMonth.bind(this, 1)} className={'testClass'}>next</a>
+        <h1>{this.props.monthnum[monthCurrent].name}</h1>
+        <DayNames/>
+        {/*<h2>{this.state.currentDate}</h2>*/}
+        {/*<h1 days={days}>{thismonth}</h1>*/}
+
+        {/*<Calendar days={this.state.days} day={this.state.day}/>*/}
+        <div className='calendar'>{daysArray}</div>
       </div>
     );
   }
 });
 
-var Calendar = React.createClass({
+// var Calendar = React.createClass({
+//
+//     render: function() {
+//       var days = this.props.days;
+//       var day = this.props.day;
+//
+//       // let daysArray = [];
+//       //
+//       //   var dayOffset = this.props.day;
+//       //   if (dayOffset == 0) {
+//       //      dayOffset = 7;
+//       //   }
+//       //
+//       //
+//       //   for (var i = 2; i <= dayOffset; i++) {
+//       //     daysArray.push(
+//       //       <Day myDay='00' key={100+i}></Day>
+//       //     );
+//       //   }
+//       //
+//       //   for (var i = 1; i <= this.props.days; i++) {
+//       //     var myDay = {
+//       //         number: i,
+//       //     };
+//       //     daysArray.push(
+//       //       <Day myDay={myDay} key={i}></Day>
+//       //     );
+//       //   }
+//       //
+//       //   console.log (daysArray);
+//
+//         return (
+//           <div className="row month">
+//             {/*{daysArray}*/}
+//           </div>
+//         );
+//     }
+//
+// });
 
-    render: function() {
-      var days = this.props.days;
-      var day = this.props.day;
+// var Day = React.createClass({
+//
+//   render: function () {
+//     var dayEvent = this.props.myDay.number
+//     // var eventTitle = myEvents['0'][dayEvent];
+//     // if (eventTitle) {
+//     //   console.log('the title is: ' + eventTitle.title);
+//     //   this.props.myDay.eventTitle = eventTitle.title;
+//     //   this.props.myDay.eventStatus = eventTitle.status;
+//     //   this.props.myDay.eventLink = eventTitle.link;
+//     // }
+//     // console.log(eventTitle);
+//     if (dayEvent) {
+//         this.props.myDay.mydate = this.props.myDay.number +'.'+;
+//     }
+//     return (
+//       <div className={'single-day eventstatus'+this.props.myDay.eventStatus}>
+//         <span>{this.props.myDay.number}</span>
+//         {/*{ eventTitle && <a href={this.props.myDay.eventLink}>{this.props.myDay.eventTitle}</a> }        */}
+//       </div>
+//     )
+//   }
+//
+// });
 
-      let daysArray = [];
-
-        var dayOffset = this.props.day;
-        if (dayOffset == 0) {
-           dayOffset = 7;
-        }
-
-
-        for (var i = 2; i <= dayOffset; i++) {          
-          daysArray.push(
-            <Day myDay='00' key={100+i}></Day>
-          );
-        }
-
-        for (var i = 1; i <= this.props.days; i++) {
-          var myDay = {
-              number: i,
-          };
-          daysArray.push(
-            <Day myDay={myDay} key={i}></Day>
-          );
-        }
-
-        console.log (daysArray);
-
-        return (
-          <div className="row month">
-            {daysArray}
-          </div>
-        );        
-    }
-
-});
-
-var Day = React.createClass({
-
-  render: function () {
-    var dayEvent = this.props.myDay.number
-    var eventTitle = myEvents['0'][dayEvent];
-    if (eventTitle) {
-      console.log('the title is: ' + eventTitle.title);
-      this.props.myDay.eventTitle = eventTitle.title;
-      this.props.myDay.eventStatus = eventTitle.status;
-      this.props.myDay.eventLink = eventTitle.link;
-    }
-    console.log(eventTitle);
-    return (
-      <div className={'single-day eventstatus'+this.props.myDay.eventStatus}>      
-        <span>{this.props.myDay.number}</span>
-        { eventTitle && <a href={this.props.myDay.eventLink}>{this.props.myDay.eventTitle}</a> }        
-      </div>
-    )
-  }
-
+const DayNames = React.createClass({
+   render: function () {
+     return (
+        <div className='dayNames'>
+          <div className='dayName'>Mon</div>
+          <div className='dayName'>Tue</div>
+          <div className='dayName'>Wed</div>
+          <div className='dayName'>Thu</div>
+          <div className='dayName'>Fri</div>
+          <div className='dayName'>Sat</div>
+          <div className='dayName'>Sun</div>
+        </div>
+     )
+   }
 });
 
 ReactDOM.render(
